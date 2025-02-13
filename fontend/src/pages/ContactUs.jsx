c6ae8b28adfd3914de7e4ef59bb036b354392db0
shital <shitalmeshram61088@gmail.com>
2025-02-13 15:34:32 +0530

    Final project

---
+++ fontend/src/pages/ContactUs.jsx
@@ -0,0 +1,47 @@
+
+const ContactUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+          📍 Our Address :<br></br>
+    🏢 Shimmer Shop Headquarters  <br></br>
+    123, Jewellery Plaza, Main Street, <br></br>
+    Mumbai, India – 400001  <br></br>
+    <br></br>
+    📞 Phone: +91 XXXXXXXXXX  <br></br>
+    ✉ Email: support@shimmershop.com <br></br> 
+    🌐 Website: [www.shimmershop.com]  <br></br>
+    
+    <hr></hr>
+    
+    📞 Get in Touch :<br></br>
+    📌 Customer Support: <br></br>
+    Need help with an order? Have a query about our products? Our support team is available to assist you. <br></br> 
+    <br></br>
+    🕒 Working Hours:<br></br>
+    Monday – Saturday: 9:00 AM – 7:00 PM<br></br>
+    Sunday: Closed<br></br>
+    <br></br>
+    📞 Call Us: +91 XXXXXXXXXX  <br></br>
+    📧 Email Us: support@shimmershop.com  <br></br>
+    <hr></hr>
+    📦 Order & Shipping Queries :<br></br>
+    For any inquiries regarding your order status, delivery tracking, or returns, contact our order management team:  <br></br>
+    📧 orders@shimmershop.com  <br></br>
+    <hr></hr>
+    💼 Business Inquiries :<br></br>
+    Interested in partnering with us or featuring your jewellery brand on our platform?  <br></br>
+    📧 business@shimmershop.com  <br></br>
+    <hr></hr>
+    📲 Connect With Us on Social Media :<br></br>
+    Stay updated on new arrivals, exclusive offers, and jewellery trends!<br></br>  
+    <br></br>
+    🔹 Facebook: [facebook.com/shimmershop] <br></br>
+    🔹 Instagram: [instagram.com/shimmershop] <br></br>
+    🔹 Twitter: [twitter.com/shimmershop] <br></br>
+      </b>
+    </div>
+  );
+};
+
+export default ContactUs;
